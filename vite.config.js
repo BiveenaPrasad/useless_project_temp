acbedc06c6d4356c2b7867cc48b720721045ee6d
samgeorge-mj <georgesam460@gmail.com>
2026-09-12 06:50:55 +0530

    Remove base path for Vercel deployment

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,6 @@
 import { defineConfig } from 'vite'
 
 export default defineConfig({
-  base: '/spillup/', // Required for GitHub Pages deployment
   server: {
     allowedHosts: true // Allows all hosts, including localtunnel
   }
